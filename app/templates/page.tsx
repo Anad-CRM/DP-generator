5f8f59a5b242b4390331ad73e3218286fe673f52
Jagan Prasad <jaganprasad093@gmail.com>
2026-05-23 12:06:49 +0530

    style: remove unnecessary trailing whitespace from template mapping function

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -112,7 +112,7 @@ export default function TemplatesPage() {
                   </div>
                 </Link>
               );
-            })}
+            })} 
           </div>
         </main>
       </div>
